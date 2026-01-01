cbe326c18fe24f69db6ab6b503b7d86637163a5f
Sitharth S <sithusitharth03@gmail.com>
2026-01-01 22:26:40 +0530

    app update

--- app1.py
+++ app1.py
@@ -175,5 +175,5 @@ def chat():
     response = get_answer(user_message, articles)
     return jsonify({'response': response})
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
